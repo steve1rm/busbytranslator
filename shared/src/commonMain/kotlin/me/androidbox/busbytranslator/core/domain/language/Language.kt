4bd03803e5c2dbd2b9c8191017e58dea4e5781b4
Steve <steve@kitkat@gmail.com>
2023-08-05 12:53:14 +0700

    Adds langauge enum class

---
+++ shared/src/commonMain/kotlin/me/androidbox/busbytranslator/core/domain/language/Language.kt
@@ -0,0 +1,43 @@
+package me.androidbox.busbytranslator.core.domain.language
+
+enum class Language(
+    val languageCode: String,
+    val languageName: String
+) {
+    ENGLISH("en", "English"),
+    ARABIC("ar", "Arabic"),
+    AZERBAIJANI("az", "Azerbaijani"),
+    CHINESE("zh", "Chinese"),
+    CZECH("cs", "Czech"),
+    DANISH("da", "Danish"),
+    DUTCH("nl", "Dutch"),
+    FINNISH("fi", "Finnish"),
+    FRENCH("fr", "French"),
+    GERMAN("de", "German"),
+    GREEK("el", "Greek"),
+    HEBREW("he", "Hebrew"),
+    HINDI("hi", "Hindi"),
+    HUNGARIAN("hu", "Hungarian"),
+    INDONESIAN("id", "Indonesian"),
+    IRISH("ga", "Irish"),
+    ITALIAN("it", "Italian"),
+    JAPANESE("ja", "Japanese"),
+    KOREAN("ko", "Korean"),
+    PERSIAN("fa", "Persian"),
+    POLISH("pl", "Polish"),
+    PORTUGUESE("pt", "Portuguese"),
+    RUSSIAN("ru", "Russian"),
+    SLOVAK("sk", "Slovak"),
+    SPANISH("es", "Spanish"),
+    SWEDISH("sv", "Swedish"),
+    TURKISH("tr", "Turkish"),
+    UKRAINIAN("uk", "Ukrainian");
+
+    companion object {
+        fun byCode(languageCode: String): Language {
+            return entries.find { language ->
+                language.languageCode == languageCode
+            } ?: throw IllegalArgumentException("Cannot find language code or code is invalid")
+        }
+    }
+}
