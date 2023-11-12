90d14d81d6fbf9840dbee841be101aff5be2661f
Steve <steve@kitkat@gmail.com>
2023-11-12 22:04:53 +0700

    Adds themes, colors, font family, typegraphy

---
+++ androidApp/src/main/java/me/androidbox/busbytranslator/android/core/theme/Color.kt
@@ -0,0 +1,31 @@
+package me.androidbox.busbytranslator.android.core.theme
+
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+import me.androidbox.busbytranslator.core.presentation.Colors
+
+val AccentViolet = Color(Colors.AccentViolet)
+val LightBlue = Color(Colors.LightBlue)
+val LightBlueGrey = Color(Colors.LightBlueGrey)
+val TextBlack = Color(Colors.TextBlack)
+val DarkGrey = Color(Colors.DarkGrey)
+
+
+val lightColors = lightColors(
+    primary = AccentViolet,
+    background = LightBlueGrey,
+    onPrimary = Color.White,
+    onBackground = TextBlack,
+    surface = Color.White,
+    onSurface = TextBlack
+)
+
+val darkColors = darkColors(
+    primary = AccentViolet,
+    background = DarkGrey,
+    onPrimary = Color.White,
+    onBackground = Color.White,
+    surface = DarkGrey,
+    onSurface = Color.White
+)
